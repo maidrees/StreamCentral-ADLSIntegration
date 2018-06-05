e1e7d9b8eb38d1798507de5a324ce63819a3044f
Idrees Mohammed <maidrees@gmail.com>
2018-06-05 16:32:49 +0100

    Modified the default namespace

--- StreamCentral ADLS Integration/Program.cs
+++ StreamCentral ADLS Integration/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StreamCentral_ADLS_Integration
+namespace StreamCentral.ADLSIntegration
 {
     class Program
     {
