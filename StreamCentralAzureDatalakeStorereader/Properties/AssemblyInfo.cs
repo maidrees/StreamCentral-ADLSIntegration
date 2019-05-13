ee065729c8fde367cacbe3ae107e0bab73247668
Mohammed Idrees <maidrees@outlook.com>
2019-05-13 18:07:59 +0100

    Project Names

--- StreamCentralAzureDatalakeStorereader/Properties/AssemblyInfo.cs
+++ StreamCentralAzureDatalakeStorereader/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AzureDatalakeStorereader")]
+[assembly: AssemblyTitle("StreamCentral ADLS FileConnector")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AzureDatalakeStorereader")]
+[assembly: AssemblyProduct("StreamCentral ADLS FileConnector")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
